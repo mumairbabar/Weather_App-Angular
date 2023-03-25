58dfe19fc89f24ef647f251058421f1a4be0f39d
Umair Babar <mo.umairbabar@gmail.com>
2023-03-25 21:12:09 +0500

    http

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production:false,
+  weatherApiBaseUrl: 'https://weather1395.p.rapidapi.com/temperature',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'weather1395.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'95652ff7afmsh9e2f444bae19fa8p17e3f7jsnaf7e964c2f5d'
+};
